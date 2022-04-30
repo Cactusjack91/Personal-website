4f0d1f0fe68d89a73fe2b23af9495c936888b2c4
Cactusjack91 <twerklikemiley13@gmail.com>
2022-05-01 00:20:46 +0800

    about page added

--- js/third.js
+++ js/third.js
@@ -142,7 +142,7 @@ const showMenu= (toggleId, navId) =>{
   /*===== GSAP ANIMATION =====*/ 
   gsap.from(".home__img", {opacity:0,duraction:2, delay:0.2, x:60})
   gsap.from(".home__data", {opacity:0,duraction:2, delay:0.8, y:25})
-  gsap.from(".home__greeting, .home__name, .home__professional , .home__button", {opacity:0,duraction:2, delay:1, y:25, ease:'expo.out', stagger:.2})
+  gsap.from(".home__greeting, .home__name, .home__professional , .home__button, .doodle", {opacity:0,duraction:2, delay:1, y:25, ease:'expo.out', stagger:.2})
   gsap.from(".nav__logo, .nav__toggle", {opacity:0,duraction:2, delay:1.5, y:25, ease:'expo.out', stagger:.2})
   gsap.from(".nav__item", {opacity:0,duraction:2, delay:1.8, y:25, ease:'expo.out', stagger:.2})
   gsap.from(".home__social-icon", {opacity:0,duraction:2, delay:2.3, y:25, ease:'expo.out', stagger:.2})
